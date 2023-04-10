82761a65390097fc352b6c062e0ecee29756f6f9
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-04-10 16:15:12 +0200

    bugfix in renaming discussions

--- app.py
+++ app.py
@@ -276,7 +276,7 @@ GPT4All:Welcome! I'm here to assist you with anything you need. What can I do fo
         data = request.get_json()
         discussion_id = data["id"]
         title = data["title"]
-        self.current_discussion.rename(discussion_id, title)
+        self.current_discussion.rename(title)
         return "renamed successfully"
 
     def restore_discussion(self, full_message):
