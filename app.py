a0666dde558a6548cf44209e60ad2a2440cd5578
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-04-11 12:50:07 +0200

    added banner

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+######
+# Project       : GPT4ALL-UI
+# Author        : ParisNeo with the help of the community
+# Supported by Nomic-AI
+# Licence       : Apache 2.0
+# Description   : 
+# A front end Flask application for llamacpp models.
+# The official GPT4All Web ui
+# Made by the community for the community
+######
+
 import argparse
 import json
 import re
@@ -344,7 +355,7 @@ GPT4All:Welcome! I'm here to assist you with anything you need. What can I do fo
         app.config['executor'].submit(self.prepare_a_new_chatbot)
 
         self.full_message =""
-        
+
         # Return a success response
         return json.dumps({"id": self.current_discussion.discussion_id, "time": timestamp})
 
