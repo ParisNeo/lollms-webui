83b6028d18f8142a1fc760de111bb9a088933816
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-04-20 20:57:12 +0200

    upgraded code

--- pyGpt4All/backends/__init__.py
+++ pyGpt4All/backends/__init__.py
@@ -1,4 +1,4 @@
-from pyGpt4All.backends.llamacpp import LLAMACPP
+from pyGpt4All.backends.llama_cpp import LLAMACPP
 from pyGpt4All.backends.gpt_j import GPT_J
 BACKENDS_LIST={
     "llama_cpp":LLAMACPP,
