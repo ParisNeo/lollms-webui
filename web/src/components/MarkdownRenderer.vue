3834c8b211b1aa744637af003d2648a81df1eafe
ParisNeo <aloui.seifeddine@gmail.com>
2023-05-07 10:32:26 +0200

    Built new code

--- web/src/components/MarkdownRenderer.vue
+++ web/src/components/MarkdownRenderer.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+    <div :class="['markdown', isDarkMode ? 'dark' : 'light']">
       <div v-html="renderedMarkdown"></div>
     </div>
-</template>
+  </template>
   
-<script>
+  <script>
   import MarkdownIt from 'markdown-it';
   import emoji from 'markdown-it-emoji';
   
@@ -13,6 +13,10 @@
       markdownText: {
         type: String,
         required: true
+      },
+      isDarkMode: {
+        type: Boolean,
+        default: false
       }
     },
     data() {
@@ -21,15 +25,43 @@
       };
     },
     mounted() {
-      const markdownIt = new MarkdownIt().use(emoji);
-      this.renderedMarkdown = markdownIt.render(this.markdownText);
+      this.renderMarkdown();
     },
     watch: {
       markdownText(newText) {
+        this.renderMarkdown(newText);
+      },
+      isDarkMode() {
+        this.renderMarkdown();
+      }
+    },
+    methods: {
+      renderMarkdown(text) {
         const markdownIt = new MarkdownIt().use(emoji);
-        this.renderedMarkdown = markdownIt.render(newText);
+        this.renderedMarkdown = markdownIt.render(text || this.markdownText);
       }
     }
   };
-</script>
+  </script>
+  
+  <style scoped>
+  .markdown {
+    /* Add Tailwind CSS classes for general styling */
+    padding: 1rem;
+    box-shadow: sm;
+    border-radius: 5px;
+  }
+  
+  .light {
+    /* Add Tailwind CSS classes for light mode */
+    background-color: #ffffff;
+    color: #000000;
+  }
+  
+  .dark {
+    /* Add Tailwind CSS classes for dark mode */
+    background-color: #1a202c;
+    color: #ffffff;
+  }
+  </style>
   
