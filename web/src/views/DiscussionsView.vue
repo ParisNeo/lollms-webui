0bca5e60fbbdf092261b14cc01762cb8159ec94e
AndzejsP <andzejs.poprockis@gmail.com>
2023-05-18 17:42:34 +0300

    added export discussion and spinner

--- web/src/views/DiscussionsView.vue
+++ web/src/views/DiscussionsView.vue
@@ -29,7 +29,21 @@
                     type="button" @click="isSearch = !isSearch" :class="isSearch ? 'text-secondary' : ''">
                     <i data-feather="search"></i>
                 </button>
-
+                <div v-if="loading" title="Loading.." class="flex flex-row flex-grow justify-end">
+                        <!-- SPINNER -->
+                        <div role="status">
+                            <svg aria-hidden="true" class="w-6 h-6   animate-spin  fill-secondary" viewBox="0 0 100 101"
+                                fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                    fill="currentColor" />
+                                <path
+                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                    fill="currentFill" />
+                            </svg>
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                    </div>
             </div>
             <!-- SEARCH BAR -->
             <div class="flex-row  items-center gap-3 flex-0 w-full">
@@ -65,7 +79,8 @@
 
                     <div v-if="selectedDiscussions.length > 0" class="flex gap-3">
                         <!-- DELETE MULTIPLE -->
-                        <button v-if="!showConfirmation" class="flex mx-3 flex-1 text-2xl hover:text-red-600 duration-75 active:scale-90 "
+                        <button v-if="!showConfirmation"
+                            class="flex mx-3 flex-1 text-2xl hover:text-red-600 duration-75 active:scale-90 "
                             title="Remove selected" type="button" @click.stop="showConfirmation = true">
                             <i data-feather="trash"></i>
                         </button>
@@ -85,7 +100,7 @@
                     <div class="flex gap-3">
 
                         <button class="text-2xl hover:text-secondary duration-75 active:scale-90 rotate-90"
-                            title="Export selected to a file" type="button">
+                            title="Export selected to a file" type="button" @click.stop="exportDiscussions">
                             <i data-feather="log-out"></i>
                         </button>
                         <button class="text-2xl hover:text-secondary duration-75 active:scale-90 " title="Select All"
@@ -316,6 +331,23 @@ export default {
                 }
             } catch (error) {
                 console.log("Error: Could not update message", error)
+                return {}
+            }
+        },
+        async export_multiple_discussions(discussionIdArr) {
+            try {
+                if (discussionIdArr.length > 0) {
+                    const res = await axios.post('/export_multiple_discussions', {
+                        discussions: discussionIdArr
+                    })
+
+                    if (res) {
+                        return res.data
+                    }
+                }
+
+            } catch (error) {
+                console.log("Error: Could not export multiple discussions", error)
                 return {}
             }
         },
@@ -761,12 +793,65 @@ export default {
         closeToast() {
             this.showToast = false
         },
+        saveJSONtoFile(jsonData, filename) {
+            filename = filename || "data.json"
+            const a = document.createElement("a");
+            a.href = URL.createObjectURL(new Blob([JSON.stringify(jsonData, null, 2)], {
+                type: "text/plain"
+            }));
+            a.setAttribute("download", filename);
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        },
+        async exportDiscussions() {
+            // Export selected discussions
+
+            const discussionIdArr = this.list.filter((item) => item.checkBoxValue == true).map((item) => {
+                return item.id
+            })
+
+            if (discussionIdArr.length > 0) {
+                console.log("export", discussionIdArr)
+                let dateObj = new Date()
+
+                const year = dateObj.getFullYear();
+                const month = (dateObj.getMonth() + 1).toString().padStart(2, "0");
+                const day = dateObj.getDate().toString().padStart(2, "0");
+                const hours = dateObj.getHours().toString().padStart(2, "0");
+                const minutes = dateObj.getMinutes().toString().padStart(2, "0");
+                const seconds = dateObj.getSeconds().toString().padStart(2, "0");
+                const formattedDate =
+                    year +
+                    "." +
+                    month +
+                    "." +
+                    day +
+                    "." +
+                    hours +
+                    "" +
+                    minutes +
+                    "" +
+                    seconds;
+
+                const filename = 'discussions_export_'+formattedDate+'.json'
+                this.loading=true
+                const res = await this.export_multiple_discussions(discussionIdArr)
+                
+                if(res.data){
+                    this.saveJSONtoFile(res.data,filename)
+                  
+                }
+                this.loading=false
+            }
+
+        }
 
 
     },
     async created() {
         // Constructor
-        //const chime = require('../assets/chime_aud.wav')
+   
 
 
         this.setPageTitle()
