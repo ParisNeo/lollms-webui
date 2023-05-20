029672c155fcdb1aad23a59e44158b5e251db7e8
AndzejsP <andzejs.poprockis@gmail.com>
2023-05-20 14:18:08 +0300

    fixed center text and items

--- web/src/components/Toast.vue
+++ web/src/components/Toast.vue
@@ -3,7 +3,7 @@
         <div id="toast-success"
             class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
             role="alert">
-            <div class="flex flex-row">
+            <div class="flex flex-row  items-center">
                 <slot>
                 <div v-if="success"
                     class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
@@ -14,7 +14,7 @@
                     <i data-feather="x"></i>
                     <span class="sr-only">Cross icon</span>
                 </div>             
-                <span class="ml-3 text-sm font-normal whitespace-pre-wrap">{{ message }}</span>
+                <div class="ml-3 text-sm font-normal whitespace-pre-wrap">{{ message }}</div>
 
                 </slot>
             </div>
