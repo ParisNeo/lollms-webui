fd3b0dede9c6c313184a22c14fe5a7a57efb6799
AndzejsP <andzejs.poprockis@gmail.com>
2023-05-25 12:47:20 +0300

    small fix

--- web/src/views/SettingsView.vue
+++ web/src/views/SettingsView.vue
@@ -214,7 +214,7 @@
                 <div :class="{ 'hidden': mc_collapsed }" class="flex flex-col mb-2 p-2">
                     <div class="m-2">
 
-                        <div class="flex flex-row gap-3 items-center">
+                        <div class="flex flex-row gap-2 items-center">
 
                             <input id="override-model-parameters" type="checkbox"
                                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
