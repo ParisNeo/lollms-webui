d2ae6c8949577aa2900b26eb486b038c6973182f
saloui <saifeddine.aloui@cea.fr>
2023-07-21 15:39:40 +0200

    added update for macos

---
+++ macos_update.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+echo "Starting LOLLMS Web UI..."
+echo "      ___       ___           ___       ___       ___           ___      "
+echo "     /\__\     /\  \         /\__\     /\__\     /\__\         /\  \     "
+echo "    /:/  /    /::\  \       /:/  /    /:/  /    /::|  |       /::\  \    "
+echo "   /:/  /    /:/\:\  \     /:/  /    /:/  /    /:|:|  |      /:/\ \  \   "
+echo "  /:/  /    /:/  \:\  \   /:/  /    /:/  /    /:/|:|__|__   _\:\~\ \  \  "
+echo " /:/__/    /:/__/ \:\__\ /:/__/    /:/__/    /:/ |::::\__\ /\ \:\ \ \__\ "
+echo " \:\  \    \:\  \ /:/  / \:\  \    \:\  \    \/__/~~/:/  / \:\ \:\ \/__/ "
+echo "  \:\  \    \:\  /:/  /   \:\  \    \:\  \         /:/  /   \:\ \:\__\   "
+echo "   \:\  \    \:\/:/  /     \:\  \    \:\  \       /:/  /     \:\/:/  /   "
+echo "    \:\__\    \::/  /       \:\__\    \:\__\     /:/  /       \::/  /    "
+echo "     \/__/     \/__/         \/__/     \/__/     \/__/         \/__/     "
+echo " By ParisNeo"
+
+cd "$(dirname "$0")"
+
+# Better isolation for virtual environment
+CONDA_SHLVL=""
+PYTHONNOUSERSITE=1
+PYTHONPATH=""
+PYTHONHOME=""
+TEMP="./installer_files/temp"
+TMP="./installer_files/temp"
+INSTALL_ENV_DIR="./installer_files/lollms_env"
+MINICONDA_DIR="./installer_files/miniconda3"
+
+if [ ! -f "$MINICONDA_DIR/bin/activate" ]; then
+    echo "Miniconda not found."
+    exit 1
+fi
+
+source "$MINICONDA_DIR/bin/activate" "$INSTALL_ENV_DIR"
+cd lollms-webui
+
+echo "Pulling latest changes"
+git pull
+
+echo "Reinstalling requirements"
+pip install --upgrade -r requirements.txt
+
+# Pause at the end to keep the terminal window open
+read -rp "Press Enter to exit..."
