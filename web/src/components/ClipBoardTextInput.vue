3ca79f9909b27627f4d5e566ff5944e4c566ebe1
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-07-23 15:09:19 +0200

    added clipboardTextinput

---
+++ web/src/components/ClipBoardTextInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <input v-model="inputValue" :placeholder="placeholderText" />
+      <button @click="pasteFromClipboard"><i data-feather="clipboard"></i></button>
+    </div>
+  </template>
+  
+  <script>
+  import feather from 'feather-icons'
+  export default {
+    data() {
+      return {
+        inputValue: "",
+        placeholderText: "Enter text here",
+      };
+    },
+    mounted() {
+        console.log('mnted all chat', this.allDiscussionPersonalities)
+        nextTick(() => {
+            feather.replace()
+        })
+    },
+    methods: {
+      pasteFromClipboard() {
+        navigator.clipboard.readText().then((text) => {
+          this.inputValue = text;
+        });
+      },
+    },
+  };
+  </script>
+  
