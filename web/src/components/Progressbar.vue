f0e0a9fee93a03dda35fd3bc0c00263c950f1beb
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-07-25 22:34:46 +0200

    added progressbar

---
+++ web/src/components/Progressbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="progress-bar-container">
+      <div class="progress-bar" :style="{ width: `${progress}%` }"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      progress: {
+        type: Number,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .progress-bar-container {
+    background-color: #f0f0f0;
+    border-radius: 4px;
+    height: 8px;
+    overflow: hidden;
+  }
+  
+  .progress-bar {
+    background-color: #3498db;
+    height: 100%;
+    transition: width 0.3s ease;
+  }
+  </style>
+  
