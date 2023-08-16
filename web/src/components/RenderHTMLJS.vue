286c7fce4c6111c556f4155f60d46ff900da8cca
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-08-16 04:14:00 +0200

    Rebuilt

---
+++ web/src/components/RenderHTMLJS.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <div v-html="htmlContent"></div>
+    </div>
+</template>
+
+<script>
+export default {
+props: {
+    htmlContent: {
+    type: String,
+    required: true
+    }
+}
+}
+</script>
