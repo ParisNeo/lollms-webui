a740fe62524706e51970f0e58b7b8137814c4ffa
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-08-22 01:09:43 +0200

    fixed

--- web/src/views/PlayGroundView.vue
+++ web/src/views/PlayGroundView.vue
@@ -137,7 +137,7 @@ export default {
   mounted() {
     const text_element = document.getElementById('text_element');
     text_element.addEventListener('input', () => {
-      this.cursorPosition = text_element.selectionStart+1;
+      this.cursorPosition = text_element.selectionStart;
     });    
     text_element.addEventListener('click', () => {
       this.cursorPosition = text_element.selectionStart;
