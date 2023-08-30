a2f26b8d59c1b97d48bed9887c632a1ad5d4ef2c
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-08-30 02:35:25 +0200

    upgraded

---
+++ web/src/components/DynamicUIRenderer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <iframe ref="outputFrame" :srcdoc="code"></iframe>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      code: String, // The HTML/CSS/JavaScript code as a prop
+    },
+  };
+  </script>
+  
+  <style>
+  /* Add any styling for the component here */
+  </style>
+  
