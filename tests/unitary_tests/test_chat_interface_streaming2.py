c94fd5b0200e16f7c15dd1312dfb3e0d28760063
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-01-21 05:49:19 +0100

    upgraded lollms generation

---
+++ tests/unitary_tests/test_chat_interface_streaming2.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+url = "http://localhost:1234/lollms_generate"
+
+payload = {
+    "prompt": "Once apon a time, ",
+    "temperature": 0.1,
+    "stream": True
+}
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, data=json.dumps(payload), headers=headers, stream=True)
+
+if response.status_code == 200:
+    for response_chunk in response.iter_lines():
+        if response_chunk:
+            rc = response_chunk.decode()
+            print(rc,end="",flush=True)
+else:
+    print("Error:", response.status_code)
