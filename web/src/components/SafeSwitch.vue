c55532c1c758540abb1fcdacd2f3e0460f92ea6f
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-02-10 01:32:17 +0100

    upgraded tool

---
+++ web/src/components/SafeSwitch.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <label class="switch">
+        <input type="checkbox" v-bind:checked="isChecked" @change="$emit('update:isChecked', $event.target.checked)">
+        <span class="slider round"></span>
+      </label>
+    </div>
+  </template>
+  
+<script>
+  export default {
+    name: 'SafeSwitch',
+    props: ['isChecked'],    
+    data() {
+      return {
+      }
+    },
+    methods: {
+      toggleSwitch() {
+        // Handle the switch toggle logic here
+        console.log('Switch toggled:', this.isChecked);
+      }
+    }
+  }
+</script>
+  
