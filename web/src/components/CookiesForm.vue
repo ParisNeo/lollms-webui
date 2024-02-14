e9541dc91b5a1852de4f655ef17fe332d45d268a
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-02-15 00:34:50 +0100

    path traversal vulenerabilities fix with new pentest files

---
+++ web/src/components/CookiesForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="!consentGiven" class="cookie-banner">
+    <p>We use cookies to enhance your experience. By continuing to visit this site you agree to our use of cookies.</p>
+    <button @click="giveConsent">Accept</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      consentGiven: this.getConsent()
+    };
+  },
+  methods: {
+    giveConsent() {
+      // Set consent to true in the local storage
+      localStorage.setItem('cookieConsent', 'true');
+      this.consentGiven = true;
+    },
+    getConsent() {
+      // Check if consent has already been given
+      return localStorage.getItem('cookieConsent') === 'true';
+    }
+  }
+};
+</script>
+
+<style scoped>
+.cookie-banner {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: #f5f5f5;
+  padding: 20px;
+  text-align: center;
+  border-top: 1px solid #ccc;
+}
+</style>
