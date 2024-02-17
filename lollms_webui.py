deaf87a0f786e2ef446d310e25a2f4a4f4cb1f6f
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-02-17 03:21:21 +0100

    security fix

--- lollms_webui.py
+++ lollms_webui.py
@@ -72,7 +72,7 @@ def terminate_thread(thread):
         else:
             ASCIIColors.yellow("Canceled successfully")# The current version of the webui
 
-lollms_webui_version="9.1"
+lollms_webui_version="9.2"
 
 
 
