93f640c32a95455c4c62b4e71b028110e62bb723
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-02-26 23:46:03 +0100

    upgraded ui

---
+++ web/src/components/PopupViewer.vue
@@ -0,0 +1,51 @@
+<template>
+  <transition name="fade">
+    <div v-if="showPopup" class="fixed inset-0 flex items-center justify-center z-50">
+      <div class="bg-white dark:bg-gray-800 rounded shadow p-6 m-4 max-w-xs max-h-full text-center overflow-auto">
+        <button @click="hidePopup" class="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Hide
+        </button>
+        <iframe v-show="showInfo" :src="webpageUrl" class="w-full"></iframe>
+        <button @click="toggleInfo" class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Toggle Info
+        </button>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showPopup: false,
+      showInfo: false,
+      isDarkTheme: false,
+      webpageUrl: 'https://your-wordpress-site.com/some-page',
+    };
+  },
+  methods: {
+    hidePopup() {
+      this.showPopup = false;
+    },
+    toggleInfo() {
+      this.showInfo = !this.showInfo;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+.dark-theme {
+  /* Add your dark theme CSS here */
+}
+.light-theme {
+  /* Add your light theme CSS here */
+}
+</style>
