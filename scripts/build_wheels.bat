e14495b4050feba927b8d80ef975e67ee3b003c4
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-02-29 19:32:28 +0100

    fix and sync

---
+++ scripts/build_wheels.bat
@@ -0,0 +1,2 @@
+SET "CMAKE_ARGS=-DLLAMA_CUBLAS=on" && pip wheel llama-cpp-python --wheel-dir=E:/Build
+
---
+++ scripts/build_wheels_linux.sh
@@ -0,0 +1,2 @@
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip wheel llama-cpp-python --wheel-dir=~/builds
+
