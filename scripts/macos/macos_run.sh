5f68f6b0b80b7c6c94c24fa341f6f15ae0d4169d
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-04-05 00:53:23 +0200

    sync

--- scripts/macos/macos_run.sh
+++ scripts/macos/macos_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Starting LOLLMS Web UI..."
+echo Starting LOLLMS Web UI...
 echo "      ___       ___           ___       ___       ___           ___      "
 echo "     /\__\     /\  \         /\__\     /\__\     /\__\         /\  \     "
 echo "    /:/  /    /::\  \       /:/  /    /:/  /    /::|  |       /::\  \    "
@@ -13,36 +13,36 @@ echo "    \:\__\    \::/  /       \:\__\    \:\__\     /:/  /       \::/  /    "
 echo "     \/__/     \/__/         \/__/     \/__/     \/__/         \/__/     "
 echo " By ParisNeo"
 
+
 cd "$(dirname "$0")"
 
-# Better isolation for virtual environment
+# better isolation for virtual environment
 CONDA_SHLVL=""
 PYTHONNOUSERSITE=1
 PYTHONPATH=""
 PYTHONHOME=""
-TEMP="./installer_files/temp"
+miniconda_folder="./installer_files"
 TMP="./installer_files/temp"
-ENV_NAME="lollms"
-INSTALL_ENV_DIR="./installer_files/miniconda3/envs/lollms"
-MINICONDA_DIR="./installer_files/miniconda3"
 
-if [ ! -f "$MINICONDA_DIR/bin/activate" ]; then
-    echo "Miniconda not found."
-    exit 1
+if  [ -e "$miniconda_folder" ]; then
+    INSTALL_ENV_DIR="./installer_files/lollms_env"
+    MINICONDA_DIR="./installer_files/miniconda3"
+    MINICONDA_CMD="$MINICONDA_DIR/bin/activate"
+    if [ ! -f "$MINICONDA_DIR/bin/activate" ]; then
+        echo "Miniconda not found."
+        exit 1
+    fi
+else
+    INSTALL_ENV_DIR="lollms"
+    MINICONDA_CMD="conda activate"
 fi
 
-source "$MINICONDA_DIR/bin/activate" "$ENV_NAME"
+source "$MINICONDA_CMD" "$INSTALL_ENV_DIR"
 cd lollms-webui
 
-# Set default CUDA toolkit to the one in the environment
+# set default cuda toolkit to the one in the environment
 CUDA_PATH="$INSTALL_ENV_DIR"
 
-# Launch the LOLLMS Web UI application
 python app.py "$@"
 
-# Pause at the end to keep the terminal window open
 read -rp "Press Enter to exit..."
-
-
-
-
