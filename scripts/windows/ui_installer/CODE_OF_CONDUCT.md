40642d5f704740ecb63e11261db721f10e892a0b
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-04-27 02:22:54 +0200

    added installer

---
+++ scripts/windows/ui_installer/CODE_OF_CONDUCT.md
@@ -0,0 +1,43 @@
+## Code of Conduct
+
+Our chatbot is an inclusive community that values respect, collaboration, and innovation. We welcome contributors of all backgrounds and skill levels to join us in building a positive and productive community. To ensure that our community remains safe, respectful, and welcoming, we ask all contributors to abide by this code of conduct.
+
+### 1. Respectful Communication
+
+We value open communication and encourage contributors to express themselves in a respectful and constructive manner. We do not tolerate discrimination, harassment, or abuse of any kind, including but not limited to:
+
+- Offensive comments related to gender, gender identity and expression, sexual orientation, disability, mental illness, race, ethnicity, age, nationality, religion, or physical appearance
+- Threats, intimidation, or bullying
+- Inappropriate sexual advances or imagery
+
+### 2. Collaborative Development
+
+We encourage collaboration and teamwork among contributors and ask that everyone work together in a constructive and positive manner. We do not tolerate disruptive behavior, including but not limited to:
+
+- Spamming, trolling, or flaming
+- Hijacking discussions or derailing conversations
+- Refusal to consider alternative viewpoints or approaches
+
+### 3. Innovative Contributions
+
+We welcome contributions of all kinds and encourage innovation and experimentation. However, we do not tolerate the use of our chatbot for any form of misinformation, including but not limited to:
+
+- The dissemination of false information, rumors, or hoaxes
+- The promotion of conspiracy theories or fake news
+- The use of our chatbot for malicious purposes, including but not limited to fraud, scams, or phishing
+
+### 4. Consequences of Unacceptable Behavior
+
+We take all reports of unacceptable behavior seriously and will investigate all incidents promptly and thoroughly. We reserve the right to take any action deemed necessary, including but not limited to:
+
+- Warning the individual responsible for the unacceptable behavior
+- Temporarily or permanently revoking their access to the chatbot
+- Banning them from future participation in the community
+
+### 5. Reporting Unacceptable Behavior
+
+If you experience or witness behavior that violates this code of conduct, please report it immediately to the chatbot administrator. All reports will be kept confidential and will be investigated promptly and thoroughly.
+
+### 6. Acknowledgment of Code of Conduct
+
+By contributing to our chatbot, you acknowledge that you have read and agree to abide by this code of conduct. You also acknowledge that you have the responsibility to report any violations of this code of conduct.
---
+++ scripts/windows/ui_installer/first_install.bat
@@ -0,0 +1,210 @@
+@echo off
+
+@rem This script will install miniconda and git with all dependencies for this project
+@rem This enables a user to install this project without manually installing conda and git.
+
+echo "      ___       ___           ___       ___       ___           ___      "
+echo "     /\__\     /\  \         /\__\     /\__\     /\__\         /\  \     "
+echo "    /:/  /    /::\  \       /:/  /    /:/  /    /::|  |       /::\  \    "
+echo "   /:/  /    /:/\:\  \     /:/  /    /:/  /    /:|:|  |      /:/\ \  \   "
+echo "  /:/  /    /:/  \:\  \   /:/  /    /:/  /    /:/|:|__|__   _\:\~\ \  \  "
+echo " /:/__/    /:/__/ \:\__\ /:/__/    /:/__/    /:/ |::::\__\ /\ \:\ \ \__\ "
+echo " \:\  \    \:\  \ /:/  / \:\  \    \:\  \    \/__/~~/:/  / \:\ \:\ \/__/ "
+echo "  \:\  \    \:\  /:/  /   \:\  \    \:\  \         /:/  /   \:\ \:\__\   "
+echo "   \:\  \    \:\/:/  /     \:\  \    \:\  \       /:/  /     \:\/:/  /   "
+echo "    \:\__\    \::/  /       \:\__\    \:\__\     /:/  /       \::/  /    "
+echo "     \/__/     \/__/         \/__/     \/__/     \/__/         \/__/     "
+echo V8.5 (alpha)
+echo -----------------
+echo By ParisNeo
+echo -----------------
+cd
+
+@rem workaround for broken Windows installs
+set PATH=%PATH%;%SystemRoot%\system32
+
+cd /D "%~dp0"
+
+echo "%cd%"| findstr /C:" " >nul && call :PrintBigMessage "This script relies on Miniconda which can not be silently installed under a path with spaces. Please put it in a path without spaces and try again" && goto failed
+call :PrintBigMessage "WARNING: This script relies on Miniconda which will fail to install if the path is too long."
+set "SPCHARMESSAGE="WARNING: Special characters were detected in the installation path!" "         This can cause the installation to fail!""
+echo "%CD%"| findstr /R /C:"[!#\$%&()\*+,;<=>?@\[\]\^`{|}~]" >nul && (
+  call :PrintBigMessage %SPCHARMESSAGE%
+)
+set SPCHARMESSAGE=
+
+cls
+
+
+@rem better isolation for virtual environment
+SET "CONDA_SHLVL="
+SET PYTHONNOUSERSITE=1
+SET "PYTHONPATH="
+SET "PYTHONHOME="
+SET "TEMP=%cd%\installer_files\temp"
+SET "TMP=%cd%\installer_files\temp"
+
+
+IF EXIST "installer_files" (
+    echo Removing folder: installer_files. Please wait ...
+    RMDIR /S /Q "installer_files"
+)
+
+IF EXIST "lollms-webui" (
+    echo Removing folder: lollms-webui. Please wait ...
+    RMDIR /S /Q "lollms-webui"
+)
+
+set MINICONDA_DIR=%cd%\installer_files\miniconda3
+set INSTALL_ENV_DIR=%cd%\installer_files\lollms_env
+set MINICONDA_DOWNLOAD_URL=https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
+set REPO_URL=https://github.com/ParisNeo/lollms-webui.git
+
+set "PACKAGES_TO_INSTALL=python=3.11 git pip"
+
+if not exist "%MINICONDA_DIR%\Scripts\conda.exe" (
+  @rem download miniconda
+  echo Downloading Miniconda installer from %MINICONDA_DOWNLOAD_URL%
+  call curl -LO "%MINICONDA_DOWNLOAD_URL%"
+
+  @rem install miniconda
+  echo. && echo Installing Miniconda To "%MINICONDA_DIR%" && echo Please Wait... && echo.
+  start "" /W /D "%cd%" "Miniconda3-latest-Windows-x86_64.exe" /InstallationType=JustMe /NoShortcuts=1 /AddToPath=0 /RegisterPython=0 /NoRegistry=1 /S /D=%MINICONDA_DIR% || ( echo. && echo Miniconda installer not found. && goto failed )
+  del /q "Miniconda3-latest-Windows-x86_64.exe"
+  if not exist "%MINICONDA_DIR%\Scripts\activate.bat" ( echo. && echo Miniconda install failed. && goto end )
+)
+
+echo Activating conda environment
+
+@rem activate miniconda
+call "%MINICONDA_DIR%\Scripts\activate.bat" || ( echo Miniconda hook not found. && goto end )
+
+
+echo Creating new environment
+
+@rem create the installer env
+if not exist "%INSTALL_ENV_DIR%" (
+  echo Packages to install: %PACKAGES_TO_INSTALL%
+  call conda create --no-shortcuts -y -k -p "%INSTALL_ENV_DIR%" %CHANNEL% %PACKAGES_TO_INSTALL% || ( echo. && echo Conda environment creation failed. && goto end )
+)
+
+@rem check if conda environment was actually created
+if not exist "%INSTALL_ENV_DIR%\python.exe" ( echo. && echo Conda environment is empty. && goto end )
+
+@rem activate installer env
+call conda activate "%INSTALL_ENV_DIR%" || ( echo. && echo Conda environment activation failed. && goto end )
+
+
+echo "Cloning lollms-webui"
+git clone --depth 1  --recurse-submodules https://github.com/ParisNeo/lollms-webui.git
+git submodule update --init --recursive
+cd lollms-webui\lollms_core
+pip install -e .
+cd ..
+cd utilities\safe_store
+pip install -e .
+cd ..\..
+
+pip install -r requirements.txt
+
+@rem create launcher
+if exist ..\win_run.bat (
+    echo Win run found
+) else (
+  copy scripts\windows\win_run.bat ..\
+)
+
+if exist ..\win_update.bat (
+    echo Win update found
+) else (
+  copy scripts\windows\win_update.bat ..\
+)
+
+
+if exist ..\win_conda_session.bat (
+    echo win conda session script found
+) else (
+  copy scripts\windows\win_conda_session.bat ..\
+)
+
+if exist ..\win_update_models.bat (
+    echo Win update models found
+) else (
+  copy scripts\windows\win_update_models.bat ..\
+)
+
+setlocal enabledelayedexpansion
+
+endlocal
+
+goto end
+
+:PrintBigMessage
+echo. && echo.
+echo *******************************************************************
+for %%M in (%*) do echo * %%~M
+echo *******************************************************************
+echo. && echo.
+exit /b
+goto end
+:failed
+echo Install failed
+goto endend
+:end
+
+echo Creating a bin dir (required for llamacpp binding)
+md ../installer_files/lollms_env/bin
+
+echo Preparing lollms ...
+if exist ..\personal_data (
+    echo Personal data found
+) else (
+  md ..\personal_data
+)
+echo lollms_path: lollms_core\lollms > global_paths_cfg.yaml
+echo lollms_personal_path: ..\personal_data >> global_paths_cfg.yaml
+
+set option=%1
+
+if "%option%"=="--elf" (
+    echo Installing elf binding
+    call python zoos/bindings_zoo/elf/__init__.py
+) else if "%option%"=="--openrouter" (
+    echo Installing open router binding
+    call python zoos/bindings_zoo/open_router/__init__.py
+) else if "%option%"=="--openai" (
+    echo Installing open ai binding
+    call python zoos/bindings_zoo/open_ai/__init__.py
+) else if "%option%"=="--groq" (
+    echo Installing groq binding
+    call python zoos/bindings_zoo/groq/__init__.py
+) else if "%option%"=="--mistralai" (
+    echo Installing mistral ai binding
+    call python zoos/bindings_zoo/mistral_ai/__init__.py
+) else if "%option%"=="--ollama" (
+    echo Installing ollama binding
+    call python zoos/bindings_zoo/ollama/__init__.py
+) else if "%option%"=="--vllm" (
+    echo Installing vllm binding
+    call python zoos/bindings_zoo/elf/__init__.py
+) else if "%option%"=="--litellm" (
+    echo Installing litellm binding
+    call python zoos/bindings_zoo/litellm/__init__.py
+) else if "%option%"=="--exllamav2" (
+    echo Installing exllamav2 binding
+    call python zoos/bindings_zoo/bs_exllamav2/__init__.py
+) else if "%option%"=="--python_llama_cpp" (
+    echo Installing python_llama_cpp binding
+    call python zoos/bindings_zoo/python_llama_cpp/__init__.py
+) else if "%option%"=="--huggingface" (
+    echo Installing huggingface binding
+    call python zoos/bindings_zoo/huggingface/__init__.py
+) else (
+    echo No valid option selected
+)
+
+@rem cd scripts\python\lollms_installer
+@rem call python main.py
+@rem cd ..
+echo Installation complete.
+:endend
