9ff12d042b14f6f15f31800ba0b801875022d9f5
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-05-23 01:04:06 +0200

    added fun mode

---
+++ web/src/views/AutoSDView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div style="width: 100vw; height: 100vh">
+    <iframe :src="$store.state.config.sd_base_url" class="m-0 p-0 w-full h-full"></iframe>
+  </div>
+</template>
+
+<script>
+//import "../css/classic.css";
+</script>
+  
