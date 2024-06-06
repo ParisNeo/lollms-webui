6639fde748e838c98e99287e24760391d158c353
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-06-06 09:05:03 +0200

    Update lollms_webui.py

--- lollms_webui.py
+++ lollms_webui.py
@@ -72,7 +72,7 @@ def terminate_thread(thread):
         else:
             ASCIIColors.yellow("Canceled successfully")# The current version of the webui
 
-lollms_webui_version="9.8 (δ)"
+lollms_webui_version="9.8 (ε)"
 
 
 
