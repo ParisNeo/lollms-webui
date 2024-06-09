c2daccff4b4895d4249c05d592741afdb418615c
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-06-10 00:40:44 +0200

    added datasources

--- web/src/views/SettingsView.vue
+++ web/src/views/SettingsView.vue
@@ -1083,6 +1083,27 @@
                 </div>
                 <div :class="{ 'hidden': data_conf_collapsed }" class="flex flex-col mb-2 px-3 pb-0">
                 
+                    <Card title="Data Sources" :is_subcard="true" class="pb-2  m-2">
+                        <table class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <tr>
+                            <td style="min-width: 200px;">
+                            <label for="data_sources" class="text-sm font-bold" style="margin-right: 1rem;">Data Sources:</label>
+                            </td>
+                            <td style="width: 100%;">
+                            <div v-for="(source, index) in configFile.data_sources" :key="index" class="flex items-center mb-2">
+                                <input
+                                type="text"
+                                v-model="configFile.data_sources[index]"
+                                @change="settingsChanged=true"
+                                class="w-full mt-1 px-2 py-1 border border-gray-300 rounded dark:bg-gray-600"
+                                >
+                                <button @click="removeDataSource(index)" class="ml-2 px-2 py-1 bg-red-500 text-white rounded">Remove</button>
+                            </div>
+                            <button @click="addDataSource" class="mt-2 px-2 py-1 bg-blue-500 text-white rounded">Add Data Source</button>
+                            </td>
+                        </tr>
+                        </table>
+                    </Card>
                     <Card title="Data Vectorization" :is_subcard="true" class="pb-2  m-2">
                         <table class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                                         
@@ -3859,6 +3880,14 @@ export default {
         //refreshHardwareUsage()
     }, 
     methods: {
+        addDataSource() {
+        this.configFile.data_sources.push('');
+        this.settingsChanged = true;
+        },
+        removeDataSource(index) {
+        this.configFile.data_sources.splice(index, 1);
+        this.settingsChanged = true;
+        },    
         handleTemplateSelection(event) {
             console.log("handleTemplateSelection")
             const selectedOption = event.target.value;
