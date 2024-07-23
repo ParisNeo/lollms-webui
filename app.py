94155cb89ebb42bd45c64a866463cb7795c1d047
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-07-23 23:44:27 +0200

    upgraded

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if not PackageManager.check_package_installed_with_version("ascii_colors", ):
 from ascii_colors import ASCIIColors
 ASCIIColors.success("OK")
 
-expected_pipmaster_version = "0.2.2"
+expected_pipmaster_version = "0.2.3"
 ASCIIColors.yellow(f"Checking pipmaster ({expected_pipmaster_version}) ...", end="", flush=True)
 if not PackageManager.check_package_installed_with_version("pipmaster", expected_pipmaster_version):
     PackageManager.install_or_update("pipmaster")
