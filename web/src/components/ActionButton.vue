c23b3f98e56f5ef486a02575d9b2ecf687758270
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-08-14 22:15:36 +0200

    welcome to the wonder Lollms v 11

---
+++ web/src/components/ActionButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <a :href="href" @click="onClick" class="text-2xl hover:text-primary transition duration-150 ease-in-out" :title="title">
+      <i :data-feather="icon"></i>
+    </a>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      href: {
+        type: String,
+        default: '#'
+      },
+      icon: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        default: ''
+      }
+    },
+    methods: {
+      onClick(event) {
+        if (this.href === '#') {
+          event.preventDefault();
+          this.$emit('click');
+        }
+      }
+    }
+  }
+  </script>
+  
---
+++ web/src/components/StatusIcon.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :class="{'text-green-500': status, 'text-red-500': !status}" :title="status ? titleTrue : titleFalse">
+      <i v-if="icon" :data-feather="icon"></i>
+      <b v-else class="text-2xl">{{ iconTrue }}</b>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      status: Boolean,
+      icon: String,
+      iconTrue: String,
+      iconFalse: String,
+      titleTrue: String,
+      titleFalse: String,
+    }
+  }
+  </script>
+  
