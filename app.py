329378444c73b6ca64ea9b404309ee4653c7db95
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-12-26 21:46:05 +0100

    fixed lollmsvectordb

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ packages: List[Tuple[str, str]] = [
     ("freedom_search", "0.1.9"),
     ("scrapemaster", "0.2.1"),
     ("lollms_client", "0.7.7"),
-    ("lollmsvectordb", "1.3.2"),
+    ("lollmsvectordb", "1.3.3"),
 ]
 
 if not pm.is_installed("einops"):
