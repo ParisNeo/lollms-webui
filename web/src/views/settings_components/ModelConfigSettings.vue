af1658d9569cd44c2809a724d8b8aa8b960ce1c1
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-06-08 23:50:26 +0200

    fixed

--- web/src/views/settings_components/ModelConfigSettings.vue
+++ web/src/views/settings_components/ModelConfigSettings.vue
@@ -25,7 +25,7 @@
         </div>
         <div class="setting-item">
             <label for="min_n_predict" class="setting-label flex items-center">
-                Context size
+                Min n predict
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 ml-1 text-blue-400 dark:text-blue-500 cursor-help feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>
                 </label>
             <input
@@ -40,7 +40,7 @@
         </div>       
         <div class="setting-item">
             <label for="max_n_predict" class="setting-label flex items-center">
-                Context size
+                Max n predict
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 ml-1 text-blue-400 dark:text-blue-500 cursor-help feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>
                 </label>
             <input
